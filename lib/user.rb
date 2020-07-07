b1f092fdfec9e9411e73682c7ee58f3b717a28db
Dani Coleman <danicoleman00@gmail.com>
2020-07-07 08:42:33 -0600

    Complete methods

--- lib/user.rb
+++ lib/user.rb
@@ -10,4 +10,14 @@ class User
   def learn(joke)
     @jokes << joke
   end
+
+  def tell(person, joke)
+    person.learn(joke)
+  end
+
+  def joke_by_id(id)
+    self.jokes.find do |joke|
+      joke if joke.id == id
+    end
+  end
 end
